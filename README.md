ebb2699193d977469f0d774e19a4dcff35a8bf17
Ivan Zinovyev <vanyazin@gmail.com>
2018-07-24 12:17:34 +0300

    Init

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # module-reload
+
 Reload required node modules
+
+
+## Install:
+
+
+```bash
+
+  npm install module-reload
+
+```
+
+
+## Usage:
+
+
+```javascript
+
+  var reload = require("module-reload");
+
+  // Require class MyAwesomeClass
+  var MyAwesomeClass = reload("./my-awesome-class.js");
+
+  // Require class MyAwesomeClass2 and reload class MyAwesomeClass
+  var MyAwesomeClass2 = reload("./my-awesome-class2.js");
+
+  // Reload both classes: MyAwesomeClass and MyAwesomeClass2
+  reload();
+
+```
---
+++ reload.js
@@ -0,0 +1,91 @@
+var ReloadHistory = function() {
+  var modules = [];
+  var path = require("path");
+
+  this.getModules = function() {
+    return modules; 
+  }
+
+  this.resolvePath = function(module) {
+    var realPath = module;
+    if (realPath.indexOf("/") > -1) {
+      realPath = path.resolve(realPath);   
+    }
+    return realPath;
+  }
+
+  this.addModule = function(module) {
+    if (this.findModule(module) === false) {
+      modules.push(module);
+    }
+    return modules.length - 1;
+  }
+
+  this.removeModule = function(module) {
+    var index = findModule(module);
+    return modules.splice(index, 1);
+  }
+
+  this.findModule = function(module) {
+    for (var i = 0, l = modules.length; i < l; ++i) {
+      if (modules[i] == module) {
+        return i; 
+      }
+    } 
+
+    return false;
+  }
+
+  this.reloadModule = function(module) {
+    if (module) {
+      var realPath = this.resolvePath(module);
+      console.log(realPath);
+      console.log(this.requireModule(realPath));
+      if (this.requireModule(realPath)) {
+        console.log(this.requireModule(realPath));
+        console.log(realPath);
+        this.addModule(realPath); 
+      }
+    }
+    return false;
+  }
+
+  this.requireModule = function(module) {
+    try { 
+      delete require.cache[require.resolve(module)];
+      return require(module);
+    } catch(error) {
+      return false;
+    }
+  }
+
+  this.reloadModules = function() {
+    if (modules.length > 0) {
+      var module = null;
+      for (index in modules) {
+        module = modules[index]; 
+        this.reloadModule(module);
+      } 
+    }
+    return this;
+  }
+
+  this.reload = function() {
+    this.reloadModules(); 
+    var module = null;
+    for (var i = 0, l = arguments.length; i < l; ++i) {
+      module = arguments[i];
+      this.reloadModule(module);
+    }
+
+    return modules.length;
+  }
+}
+
+var reloadHistory = new ReloadHistory;
+
+var reloadModule = function(module) {
+  return reloadHistory.reload(module);
+}
+
+module.exports = reloadModule;
